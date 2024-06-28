9ffb6c0934fd4035b6beebd4a427f3b0b5a447e9
marian-tp <149566251+marian-tp@users.noreply.github.com>
2024-06-28 09:57:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -374,6 +374,8 @@ def mapa_barris (any, compra_lloguer, habitatges_turistics):
 
 
 app = dash.Dash(__name__)
+# CANVI PER RENDER
+server = app.server
 
 app.layout = html.Div([
     html.H1("Sector de l'habitatge a Barcelona"),
